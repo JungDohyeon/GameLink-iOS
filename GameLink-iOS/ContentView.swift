ea2c93915ef2f4819f94897e80cf3879e30d90b5
JungDohyeon <jdh1109ok@naver.com>
2024-09-21 02:19:18 +0900

    Initial Commit

---
+++ GameLink-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameLink-iOS
+//
+//  Created by 정도현 on 9/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameLink-iOS/GameLink_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameLink_iOSApp.swift
+//  GameLink-iOS
+//
+//  Created by 정도현 on 9/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GameLink_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
