95bc33be315dc598129041fabdc915997d27a4d8
JungDohyeon <jdh1109ok@naver.com>
2024-09-21 03:14:16 +0900

    [Feat] UserDefaults Property Wrapper

---
+++ GameLink-iOS/Data/Sources/Literals/UserDefaultsList.swift
@@ -0,0 +1,29 @@
+//
+//  UserDefaultsList.swift
+//  GameLink-iOS
+//
+//  Created by 정도현 on 9/21/24.
+//
+
+import Foundation
+
+public struct UserDefaultsList {
+  public struct Auth {
+    @UserDefaultsWrapper<String>(key: "accessToken")
+    public static var accessToken
+    
+    @UserDefaultsWrapper<String>(key: "refreshToken")
+    public static var refreshToken
+  }
+}
+
+extension UserDefaultsList {
+  public static func clearData() {
+    clearAuthData()
+  }
+  
+  public static func clearAuthData() {
+    UserDefaultsList.Auth.accessToken = nil
+    UserDefaultsList.Auth.refreshToken = nil
+  }
+}
