fdd19f92b42fe3f5a980829c5002d3befbefcdbf
JungDohyeon <jdh1109ok@naver.com>
2024-10-29 13:41:49 +0900

    [Feat] Profile - No Account View

--- GameLink-iOS/Feature/Profile/ProfileMainView.swift
+++ GameLink-iOS/Feature/Profile/ProfileMainView.swift
@@ -13,10 +13,27 @@ struct ProfileMainView: View {
   
   var body: some View {
     VStack {
-      Text("로그인 정보가 없습니다.")
+      noAccountView()
     }
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(.background1, ignoresSafeAreaEdges: .all)
+  }
+}
+
+private extension ProfileMainView {
+  
+  @ViewBuilder
+  func noAccountView() -> some View {
+    VStack(spacing: 10) {
+      Text("현재 등록된 계정이 없어요 :(")
+        .glFont(.title1)
+        .foregroundStyle(.white)
+      
+      Text("계정을 등록하시면 인게임 정보를\n한 눈에 확인할 수 있어요!")
+        .glFont(.body2)
+        .foregroundStyle(.gray1)
+    }
+    .multilineTextAlignment(.center)
   }
 }
 
