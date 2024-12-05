4bd77ecfa544e0d9847072d838f7195bfff17baa
JungDohyeon <jdh1109ok@naver.com>
2024-12-06 03:43:51 +0900

    [Feat] Swinject - Chat Assembly

---
+++ GameLink-iOS/Utils/Inject/Assembly/ChatAssembly.swift
@@ -0,0 +1,41 @@
+//
+//  ChatAssembly.swift
+//  GameLink-iOS
+//
+//  Created by 정도현 on 12/5/24.
+//
+
+import Swinject
+
+/// Presentation-Chat Assembly
+public struct ChatAssembly: Assembly {
+  
+  let coordinator: ChatCoordinator
+  
+  public func assemble(container: Container) {
+    container.register(ChattingListViewModel.self) { resolver in
+      let service = resolver.resolve(DefaultChatService.self)!
+      return ChattingListViewModel(chatService: service, coordinator: coordinator)
+    }.inObjectScope(.container) /// ViewModel이 계속 새로 생성되는 것을 방지하기 위해 Scope(.container) 설정 (SingleTon)
+    ///
+    container.register(ChattingRoomCarouselViewModel.self) { resolver in
+      let service = resolver.resolve(DefaultChatService.self)!
+      return ChattingRoomCarouselViewModel(chatService: service, coordinator: coordinator)
+    }.inObjectScope(.container) /// ViewModel이 계속 새로 생성되는 것을 방지하기 위해 Scope(.container) 설정 (SingleTon)
+    
+    container.register(ChattingListView.self) { resolver in
+      let viewModel = resolver.resolve(ChattingListViewModel.self)!
+      return ChattingListView(viewModel: viewModel)
+    }
+    
+    container.register(ChattingFilterDetailView.self) { resolver in
+      let viewModel = resolver.resolve(ChattingListViewModel.self)!
+      return ChattingFilterDetailView(viewModel: viewModel)
+    }
+    
+    container.register(ChattingRoomCarouselView.self) { (resolver, roomData: ChatroomEntity) in
+      let viewModel = resolver.resolve(ChattingRoomCarouselViewModel.self)!
+      return ChattingRoomCarouselView(viewModel: viewModel, roomData: roomData)
+    }
+  }
+}
